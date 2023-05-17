f63de59934768066cca10b5755fd34069144591f
windyaan <windyanurmaretha@gmail.com>
2023-05-17 20:20:18 +0700

    Menjalankan program chain function

--- chainfunction/chainfunction.cpp
+++ chainfunction/chainfunction.cpp
@@ -20,5 +20,8 @@ public:
 
 int main()
 {
-	//bukunya.setJudul("Matematika
+	//bukunya.setJudul("Matematika");
+	//cout << bukunya.getJudul();
+	cout << bukunya.setJudul("Matematika").getJudul(); //chain function calls
+	return 0;
 }
