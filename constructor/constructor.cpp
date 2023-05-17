c04f3c21d38255f7a8dad7e2c5c89b9f93643653
windyaan <windyanurmaretha@gmail.com>
2023-05-17 19:44:56 +0700

    Method cetak

--- constructor/constructor.cpp
+++ constructor/constructor.cpp
@@ -31,3 +31,7 @@ mahasiswa::mahasiswa(int iNim, string iNama) {
 	nim = iNim;
 	nama = iNama;
 }
+void mahasiswa::cetak() {
+	cout << endl << "Nim = " << nim << endl;
+	cout << "Nama = " << nama << endl;
+}
