a25bd6c6dc5c1d9ac7a2023e3518c727b1d282f6
windyaan <windyanurmaretha@gmail.com>
2023-05-17 15:34:04 +0700

    Method untuk menampilkan ID, Nama, NIM

--- mbStatic/mbStatic.cpp
+++ mbStatic/mbStatic.cpp
@@ -18,3 +18,10 @@ int mahasiswa::nim = 0;
 void mahasiswa::setID() {
 	id = ++nim;
 }
+
+void mahasiswa::printAll() {
+	cout << "ID = " << id << endl;
+	cout << "Nama = " << nama << endl;
+	cout << "NIM = " << nim << endl;
+	cout << endl;
+}
